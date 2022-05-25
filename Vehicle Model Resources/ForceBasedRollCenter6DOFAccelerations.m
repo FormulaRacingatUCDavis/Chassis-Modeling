8cce61b54e13882688552e70aae26a164f3a6767
Tristan-Pham <74520801+Tristan-Pham@users.noreply.github.com>
2022-05-24 17:32:10 -0700

    Update ForceBasedRollCenter6DOFAccelerations.m

--- Vehicle Model Resources/ForceBasedRollCenter6DOFAccelerations.m
+++ Vehicle Model Resources/ForceBasedRollCenter6DOFAccelerations.m
@@ -1,11 +1,17 @@
 function [LongAcc, LatAcc, YawAcc, LongAccTot, LatAccTot, VertAcc, RollAcc, PitchAcc] = ...
-    ForceBasedRollCenter6DOFAccelerations( TFx, TFy, TMz, AFx, AFy, AMz, ... % Loads
-        Wheelbase, TrackWidth, Steer, ...                         % Geometry
-        Mass, YawInertia, CoG, ...                                % Inertia
-        LongVel, LatVel, YawVel)                                 % Velocities
+    ForceBasedRollCenter6DOFAccelerations( TFx, TFy, TMz, AFx, AFy, AMz, AFz, AMy, ... % Loads
+        Wheelbase, TrackWidth, Steer, PercentFront, ...                      % Geometry
+        Front_View_Suspension_Arm, z, Side_View_Suspension_Arm, Front_View_Jacking_Angle, ...
+        Side_View_Jacking_Angle, TrackWidthTest,...
+        Front_Roll_Stiffness, Rear_Roll_Stiffness, Roll_Dampening_Coeff_Front, ...
+        Roll_Dampening_Coeff_Rear, Roll_Speed, Roll, ...
+        Pitch_Stiffness, Pitch_Dampening, Pitch, Pitch_Velocity, ...
+        Ride_Stiffness, Ride, Ride_Dampening, Ride_Velocity,...
+        Mass, YawInertia, CoG, Sprung_COG_Height, ...                        % Inertia
+        LongVel, LatVel, YawVel, Ixx, Iyy)                                             % Velocities
 %% FroceBasedRollCenter6DOFAccelerations - Force Based Roll Center Accelerations
-% Computes planar motion accelerations for a 3DOF full track chassis model
-% https://www.overleaf.com/project/5e73ad44ea180c00018cb332 (Section 2.3.2)
+% Computes force based roll center accelerations for a 6DOF full track chassis model
+% https://www.overleaf.com/project/5e73ad44ea180c00018cb332 (Section ?)
 %
 % Inputs:
 %   TFx        - (n,4 numeric) Tire Longitudinal Force {T_F_y}    [N]
@@ -32,11 +38,20 @@ function [LongAcc, LatAcc, YawAcc, LongAccTot, LatAccTot, VertAcc, RollAcc, Pitc
 %   LatAccTot  - (n,1 numeric) Total Lateral Acceleration      {a_y}      [m/s^2]
 %
 % Notes:
+% check for correct yaw acc implementation
+% Itterations need to be checked for rotational accelerations, like tire
+% forces and yaw moments at each tire
+% How do we solve for all suspension parameters, and how do they change
+% over time
+% Test Case fix
+% Vertical Acc Parameters fix, understanding ride and implementing it
+% IXX and IYY from CAD
+% Array operations
 %
 % Author(s): 
 % Tristan Pham       (atlpham@ucdavis.edu)        [Oct 2020 - ???     ]
 % 
-% Last Updated: 17-Feburary-2022
+% Last Updated: 7-May-2022
 
 %% Test Case
 if nargin == 0
@@ -47,25 +62,62 @@ if nargin == 0
     AFx = -100;
     AFy = 0;
     AMz = 0;
+    AFz = 0;
+    AMy = 0;
     
     Wheelbase  = 1.575; 
-    TrackWidth = 1.22*ones(1,2);
+    TrackWidth = 1.22;%*ones(1,2);
+    TrackWidthTest = 1.22*ones(1,2);
     Steer      = [18, 15, 1, -1];
     
-    Mass       = 275;   
-    YawInertia = 130; 
-    CoG        = [(0.47-0.5)*Wheelbase, 0, 0.25];
+    Mass         = 275;   
+    YawInertia   = 130; 
+    CoG          = [(0.47-0.5)*Wheelbase, 0, 0.25];
+    PercentFront = 0.5;
+    Sprung_COG_Height = 0;
     
     LongVel = 15;
     LatVel  = 0;
     YawVel  = 0.5;
+
+    Front_View_Suspension_Arm = 0;   %calculation
+    Side_View_Suspension_Arm = 0;
+    Front_View_Jacking_Angle = 0; % check notes with joseph
+    Side_View_Jacking_Angle = 0; % check notes with joseph
     
-    [LongAcc, LatAcc, YawAcc, LongAccTot, LatAccTot] = ...
-        ForceBasedRollCenter6DOFAccelerations( ...
-            TFx, TFy, TMz, AFx, AFy, AMz, ...
-            Wheelbase, TrackWidth, Steer, ... 
-            Mass, YawInertia, CoG, ... 
-            LongVel, LatVel, YawVel ) %#ok<NOPRT>
+    Front_Roll_Stiffness = 0;
+    Rear_Roll_Stiffness = 0;
+    Roll_Dampening_Coeff_Front = 0;
+    Roll_Dampening_Coeff_Rear = 0;
+    Roll_Speed = 0;
+    Roll = 0;
+
+    Pitch_Stiffness = 0;
+    Pitch_Dampening = 0;
+    Pitch = 0;
+    Pitch_Velocity = 0;
+
+    Ride_Stiffness = 0;
+    Ride = 0;
+    Ride_Dampening = 0;
+    Ride_Velocity = 0;
+
+    Iyy = 0;
+    Ixx = 0;
+
+    z = 0;
+    
+    [LongAcc, LatAcc, YawAcc, LongAccTot, LatAccTot, VertAcc, RollAcc, PitchAcc] = ...
+    ForceBasedRollCenter6DOFAccelerations( TFx, TFy, TMz, AFx, AFy, AMz, AFz, AMy, ...
+        Wheelbase, TrackWidth, Steer, PercentFront, ...                     
+        Front_View_Suspension_Arm, z, Side_View_Suspension_Arm, Front_View_Jacking_Angle, ...
+        Side_View_Jacking_Angle, TrackWidthTest,...
+        Front_Roll_Stiffness, Rear_Roll_Stiffness, Roll_Dampening_Coeff_Front, ...
+        Roll_Dampening_Coeff_Rear, Roll_Speed, Roll, ...
+        Pitch_Stiffness, Pitch_Dampening, Pitch, Pitch_Velocity, ...
+        Ride_Stiffness, Ride, Ride_Dampening, Ride_Velocity,...
+        Mass, YawInertia, CoG, Sprung_COG_Height, ...                        
+        LongVel, LatVel, YawVel, Ixx, Iyy)    
     
     return;
 end
@@ -74,7 +126,7 @@ end
 
 %%% Tire Positions (n,4,3 numeric)
 TirePos = cat( 3, Wheelbase/2 + [-1, -1, 1, 1].*CoG(:,1), ...
-                  [TrackWidth(:,1).*[1 -1]/2, TrackWidth(:,2).*[1 -1]/2], ...
+                  [TrackWidthTest(:,1).*[1 -1]/2, TrackWidthTest(:,2).*[1 -1]/2], ...
                   zeros( size(Steer) ) );
 
 %%% Tire Loads (n,4,3 numeric)
@@ -82,38 +134,61 @@ TireLoad = cat( 3, TFx, TFy, zeros( size(Steer) ) );
 
 %%% Tire Yaw Moment (n,1 numeric)
 TireMoment = cross(TirePos, TireLoad, 3);
-TireMoment = sum( TireMoment(:,:,3) ) + sum(TMz,2);
+TMz = sum( TireMoment(:,:,3) ) + sum(TMz,2);
 
-%%% Moments
-%Correct a or b
-RollAcc = (sum((TFx.*sind(Steer) + TFy.*cosd(Steer)).*Front_View_Suspension_Arm...
-    - (TFx.*cosd(Steer)-TFy.*sind(Steer)).*tand(Side_View_Jacking_Angle).*tw/2)...
-    - AFy .* (Sprung_COG_Height + z) - AMz - (front_roll_stiffness + rear_roll_stiffness).*roll...
-    - (roll_dampening_coeff_front + roll_dampening_coeff_rear).*roll_speed) / Ixx;
+%%%Wheelbase Calculations
+a = Wheelbase .* (1-PercentFront);
+b = Wheelbase .* PercentFront;
 
-%Correct a or b
-PitchAcc = (sum(-(TFx.*cosd(Steer) - TFy.*sind(Steer)).*Side_View_Suspension_Arm...
-    + (TFx.*sind(Steer) + TFy.*cosd(Steer)).*tand(Front_View_Jacking_Angle).* (a or b) )...
-    + AFx .* (Sprung_COG_Height + z) - AFz.*(a-L/2) - AMy ...
-    - pitch_stiffness.*pitch - pitch_dampening.*pitch_velocity) ./ Iyy;
+%%%Chassis Accelerations
 
-%Positive or negative trackwidth?
-YawAcc = (sum((TFx.*cosd(Steer) - TFy.*sind(Steer)).*(-)tw/2 ...
-    + (TFx.*sind(Steer) + TFy.*cosd(Steer)).*(a or -b) +TMz)...
-    - AFy .* (L/2 - a) - AMz) ./ Izz; 
+%%Yaw Acceleration
+%Note* in overleaf there is the option of a or b
+%Also tried to implement trackwidth correctly, don't know if I did
+%YawComputation1 = sum((TFx .* cosd(Steer) - TFy .* sind(Steer)) .* -TrackWidth ./ 2 ...
+    %+ (TFx .* sind(Steer) + TFy .* cosd(Steer)) .* (a) + TMz);
 
-%%% Chassis Accelerations
+%YawComputation2 = sum((TFx .* cosd(Steer) - TFy .* sind(Steer)) .* TrackWidth ./ 2 ...
+    %+ (TFx .* sind(Steer) + TFy .* cosd(Steer)) .* (a) + TMz);
+
+%experimenting with Array sizes
+YawComputation1 = sum((TFx .* cosd(Steer) - TFy .* sind(Steer)) .* -TrackWidth ./ 2 ...
+    + (TFx .* sind(Steer) + TFy .* cosd(Steer)) .* (a) + TMz);
+
+YawComputation2 = sum((TFx .* cosd(Steer) - TFy .* sind(Steer)) .* TrackWidth ./ 2 ...
+    + (TFx .* sind(Steer) + TFy .* cosd(Steer)) .* (a) + TMz);
+
+YawAcc = ((YawComputation1 + YawComputation2) - AFy .* (Wheelbase ./ 2 - a) - AMz) ./ YawInertia;
+
+%%Roll Acceleration 
+% What to put for z
+RollAcc = (sum((TFx .* sind(Steer) + TFy .* cosd(Steer)) .* Front_View_Suspension_Arm...
+    - (TFx .* cosd(Steer) - TFy .* sind(Steer)) .* tand(Side_View_Jacking_Angle) .* TrackWidth ./ 2)...
+    - AFy .* (Sprung_COG_Height + z) - AMz - (Front_Roll_Stiffness + Rear_Roll_Stiffness) .* Roll...
+    - (Roll_Dampening_Coeff_Front + Roll_Dampening_Coeff_Rear) .* Roll_Speed) ./ Ixx;
+
+%%Pitch Acceleration
+%Note* in overleaf there is the option of a or b
+PitchAcc = (sum(-(TFx .* cosd(Steer) - TFy .* sind(Steer)) .* Side_View_Suspension_Arm...
+    + (TFx .* sind(Steer) + TFy .* cosd(Steer)) .* tand(Front_View_Jacking_Angle).* (a) )...
+    + AFx .* (Sprung_COG_Height + z) - AFz .* (a - Wheelbase ./ 2) - AMy ...
+    - Pitch_Stiffness .* Pitch - Pitch_Dampening .* Pitch_Velocity) ./ Iyy;
+
+
+%%Longitudinal Acceleration
 LongAcc = ((sum( TFx.*cosd(Steer) - TFy.*sind(Steer), 2 ) - AFx) ./ Mass) + LatVel.*YawVel;
 
+%%Lateral Acceleration
 LatAcc  = ((sum( TFy.*cosd(Steer) + TFx.*sind(Steer), 2 ) - AFy) ./ Mass) - LongVel.*YawVel;
 
-VertAcc = (sum(kr + br.*j + (TFx.*cosd(Steer) - TFy.*sind(Steer).*tand(Side_View_Jacking_Angle)...
-    + (TFy.*cosd(Steer) + TFx.*sind(Steer)).*tan(Front_View_Jacking_Angle) - AFz) ./ Mass;
+%%Vertical Acceleration
+VertAcc = (sum(Ride_Stiffness .* Ride + Ride_Dampening .* Ride_Velocity + (TFx .* cosd(Steer) - TFy .* sind(Steer)) .* tand(Side_View_Jacking_Angle)...
+    + (TFy .* cosd(Steer) + TFx .* sind(Steer)) .* tan(Front_View_Jacking_Angle)) - AFz) ./ Mass;
 
 LongAccTot = LongAcc - LatVel .*YawVel;
 LatAccTot  = LatAcc  + LongVel.*YawVel;
 
-%%% Matricies (FIXME)
+%%% Matricies (FIXME) Other way of expressing computations
 % x_vector = [x;y;z];
 % omega_vector = [roll,yaw,pitch];
 % Is = [Ixx_s, 0, -Ixz_s; 0, Iyy_s, 0; -Izx_s, 0, Izz_s];
